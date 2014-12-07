4620fbd793c586e5db22a3913e1fc1c0c79911a2
Jeff Weber <jweber@cofront.net>
2014-12-07 03:05:33 +0000

    add setup script for initializing a new environment

---
+++ setup.sh
@@ -0,0 +1,8 @@
+mkdir ~/.dotfiles_backup
+mv ~/.bash_profile ~/.bashrc ~/.gitconfig ~/.tmux.conf ~/.vim ~/.vimrc ~/.dotfiles_backup
+ln -sf ~/.dotfiles/bash_profile ~/.bash_profile
+ln -sf ~/.dotfiles/bashrc ~/.bashrc
+ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/vim ~/.vim
+ln -sf ~/.dotfiles/vimrc ~/.vimrc

cfa31146977069c628484698f1ccbacff8d6a9f8
Jeff Weber <jweber@cofront.net>
2020-01-05 18:59:42 -0500

    create ~/.local/bin

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ ln -sf ~/.dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/.dotfiles/bashrc ~/.bashrc
 ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
+mkdir -p ~/.local/bin
